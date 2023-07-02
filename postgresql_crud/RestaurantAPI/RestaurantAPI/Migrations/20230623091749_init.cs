64e07a4f5a8507350de9c427326f44fe677ef117
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2023-07-02 17:20:50 +0600

    init

---
+++ postgresql_crud/RestaurantAPI/RestaurantAPI/Migrations/20230623091749_init.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RestaurantAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CustomerName = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodItems",
+                columns: table => new
+                {
+                    FoodItemId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FoodItemName = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItems", x => x.FoodItemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderMasters",
+                columns: table => new
+                {
+                    OrderMasterId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderNumber = table.Column<string>(type: "varchar(75)", nullable: true),
+                    CustomerId = table.Column<int>(type: "integer", nullable: false),
+                    PMethod = table.Column<string>(type: "varchar(10)", nullable: true),
+                    GTotal = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderMasters", x => x.OrderMasterId);
+                    table.ForeignKey(
+                        name: "FK_OrderMasters_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderMasterId = table.Column<long>(type: "bigint", nullable: false),
+                    FoodItemId = table.Column<int>(type: "integer", nullable: false),
+                    FoodItemPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_FoodItems_FoodItemId",
+                        column: x => x.FoodItemId,
+                        principalTable: "FoodItems",
+                        principalColumn: "FoodItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_OrderMasters_OrderMasterId",
+                        column: x => x.OrderMasterId,
+                        principalTable: "OrderMasters",
+                        principalColumn: "OrderMasterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_FoodItemId",
+                table: "OrderDetails",
+                column: "FoodItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderMasterId",
+                table: "OrderDetails",
+                column: "OrderMasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderMasters_CustomerId",
+                table: "OrderMasters",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "FoodItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderMasters");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
